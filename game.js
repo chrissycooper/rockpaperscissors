b10be05659c0d2b4a3447b10be772eea94eba5e7
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-10 21:54:45 -0600

    feat: Add Game class skeleton, update Player class

---
+++ game.js
@@ -0,0 +1,13 @@
+class Game {
+    constructor() {
+        this.playerOne = new Player('Human', '😎');
+        this.playerTwo = new Player('Computer', '💻');
+    }
+
+}
+
+//a way to keep track of the data for the game board
+//a way to keep track of the selected game type
+//a way to check the Game's board data for win conditions
+//a way to detect when a game is a draw (no one has won)
+//a way to reset the Game's board to begin a new game
--- player.js
+++ player.js
@@ -1,11 +1,16 @@
 class Player {
-    constructor(name, token) {
-        this.name = name;
-        this.token = token;
-        this.wins = 0;
-    }
+  constructor(name, token) {
+    this.name = name;
+    this.token = token;
+    this.wins = 0;
+  }
 
-    takeTurn() {
-        
+  takeTurn(choice) {
+    if (this.name === "computer") {
+      var num = Math.floor(Math.random() * 3);
+      return num;
+    } else {
+      return choice;
     }
+  }
 }
