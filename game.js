8dd600d77c377cef2c5738b98729d23b56ece941
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-11 12:35:31 -0600

    feat: Add properties to Game class

--- game.js
+++ game.js
@@ -3,11 +3,21 @@ class Game {
         this.playerOne = new Player('Human', '😎');
         this.playerTwo = new Player('Computer', '💻');
         this.gameType = gameType;
+        this.choicesEasy = ["rock", "paper", "scissors"];
+        this.choicesHard = ["final girl", "jock", "hunter", "killer", "nerd"];
+    }
+
+    checkForWinConditions() {
+
+    }
+
+    resetGame() {
+        
     }
 
 }
 
-//a way to keep track of the data for the game board - the moves the players make?
+//a way to keep track of the data for the game board - the moves the players can make, i.e. the choices
 //a way to keep track of the selected game type - easy or hard mode
 //a way to check the Game's board data for win conditions - compare the two moves against each other
 //a way to detect when a game is a draw (no one has won) - compaare the two moves against each other
