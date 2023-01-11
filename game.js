fd719e0594e350feee922c45e21770e410e48a91
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-10 22:25:56 -0600

    refactor: Add pseudocode for classes and basic game structure

--- game.js
+++ game.js
@@ -1,13 +1,16 @@
 class Game {
-    constructor() {
+    constructor(gameType) {
         this.playerOne = new Player('Human', '😎');
         this.playerTwo = new Player('Computer', '💻');
+        this.gameType = gameType;
     }
 
 }
 
-//a way to keep track of the data for the game board
-//a way to keep track of the selected game type
-//a way to check the Game's board data for win conditions
-//a way to detect when a game is a draw (no one has won)
-//a way to reset the Game's board to begin a new game
+//a way to keep track of the data for the game board - the moves the players make?
+//a way to keep track of the selected game type - easy or hard mode
+//a way to check the Game's board data for win conditions - compare the two moves against each other
+//a way to detect when a game is a draw (no one has won) - compaare the two moves against each other
+//a way to reset the Game's board to begin a new game - reset game method?
+
+//basic rock paper scissors - two values are offered up, they are compared against each other, a winner is determined
