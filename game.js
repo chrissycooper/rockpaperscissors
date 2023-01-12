f06827e4b37ce35cb21faaa2db2f3c105ad215de
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-12 10:52:14 -0600

    feat: Add object oriented game setup for easy mode

--- game.js
+++ game.js
@@ -3,27 +3,28 @@ class Game {
         this.playerOne = new Player('Human', '😎');
         this.playerTwo = new Player('Computer', '💻');
         this.gameType = gameType; //"easy" or "hard"
-        this.choicesEasy = ["rock", "paper", "scissors"];
+        this.choicesEasy = [
+            {name: "rock", beats: "scissors", losesTo: "paper"}, 
+            {name: "paper", beats: "rock", losesTo: "scissors"}, 
+            {name: "scissors", beats: "paper", losesTo: "rock"}
+        ]
         this.choicesHard = ["final girl", "jock", "hunter", "killer", "nerd"];
     }
 
 
-    //the following functions correlate the move to the index value of the choices arrays, and decide a winner based on that
+   
     checkForWinConditionsEasy() {
-        if (this.playerOne.move === 0 && this.playerTwo.move === 2 ){
-            this.playerTwo.wins += 1;
-        } else if(this.playerOne.move === 2 && this.playerTwo.move === 1) {
-            this.playerTwo.wins += 1;
-        } else if(this.playerOne.move === 1 && this.playerTwo.move === 0) {
-            this.playerTwo.wins += 1;
-        } else if(this.playerOne.move === 2 && this.playerTwo.move === 0){
-            this.playerOne.wins += 1;
-        } else if(this.playerOne.move === 1 && this.playerTwo.move === 2) {
-            this.playerOne.wins += 1;
-        } else if(this.playerOne.move === 0 && this.playerTwo.move === 1) {
-            this.playerOne.wins += 1;
-        } else if(this.playerOne.move === this.playerTwo.move) {
-            return "it's a tie";
+        // if player one puts in 2, and playerTwo produces 0
+        if (this.playerOne.move === this.playerTwo.move) {
+            return "It's a tie!"
+        } 
+        var p1MoveObj = this.choicesEasy[this.playerOne.move] //paper
+        var p2MoveObj = this.choicesEasy[this.playerTwo.move] //rock
+
+        if (p1MoveObj.name === p2MoveObj.beats) {
+            this.playerTwo.wins++;
+        } else if (p2MoveObj.name === p1MoveObj.beats) {
+            this.playerOne.wins++;
         }
     }
 
@@ -62,4 +63,21 @@ class Game {
 //a way to detect when a game is a draw (no one has won) - compaare the two moves against each other
 //a way to reset the Game's board to begin a new game - reset game method?
 
-//basic rock paper scissors - two values are offered up, they are compared against each other, a winner is determined
+//basic rock paper scissors - two values are offered up, they are compared against each other, a winner is determined\\
+
+
+// if (this.playerOne.move === 0 && this.playerTwo.move === 2 ){
+//     this.playerTwo.wins += 1;
+// } else if(this.playerOne.move === 2 && this.playerTwo.move === 1) {
+//     this.playerTwo.wins += 1;
+// } else if(this.playerOne.move === 1 && this.playerTwo.move === 0) {
+//     this.playerTwo.wins += 1;
+// } else if(this.playerOne.move === 2 && this.playerTwo.move === 0){
+//     this.playerOne.wins += 1;
+// } else if(this.playerOne.move === 1 && this.playerTwo.move === 2) {
+//     this.playerOne.wins += 1;
+// } else if(this.playerOne.move === 0 && this.playerTwo.move === 1) {
+//     this.playerOne.wins += 1;
+// } else if(this.playerOne.move === this.playerTwo.move) {
+//     return "it's a tie";
+// }
--- player.js
+++ player.js
@@ -6,9 +6,9 @@ class Player {
     this.move = null;
   }
 
-  takeTurn(choice) {
+  takeTurn(choice) {        //maybe add an if/else for game mode? this could be in main if it's easier
     if (this.name === "Computer") {
-      var num = Math.floor(Math.random() * 5);
+      var num = Math.floor(Math.random() * 3);
       this.move = num;
     } else {
       this.move = choice;
