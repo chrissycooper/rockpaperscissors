57e31a092f1b5c3d561815d9b4b932fcc9e86aa4
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-12 21:28:33 -0600

    feat: Add pseudocode

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ var game = new Game();
         //if gameType = hard, then use the hard choices
             //this is somewhat in the takeTurn() fcn
         //also the DOM will need to display the possible choices for the user depending on the game mode (which will update in the class on button click)
+
+//connect data model to DOM
+    //create variables
+    //write show/hide functions
+
+    //start with choosing game mode
+        //variable to select the whole view so it can be hidden
+            //this can also be the section that has the event listener? if event.target.id = easyMode then make the game mode easy, etc
+        //add cursor: pointer to each individual div so they know they can click whereever they please
+        //displayGameMode() --
+            //if game mode is easy then show easy mode, hide choose mode view
+    
+    //then choosing a move
+        //when emojis are clikced on the human Player's move is changed based on the click
+        //the computer will also have to takeTurn()
+        //set timeout will come in here
+
+//
