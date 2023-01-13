4afc44c1323b2885c33fe410b113a1b602de92f2
Chrissy Cooper <cooper.christine.r@gmail.com>
2023-01-12 21:03:58 -0600

    feat: Add main game boards for both modes

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 var game = new Game();
 
 //onclick of easy mode or hard mode button, then game.gameType = easy or hard depending on what was clicked
+        //if gameType = easy, then use the easy choices
+        //if gameType = hard, then use the hard choices
+            //this is somewhat in the takeTurn() fcn
+        //also the DOM will need to display the possible choices for the user depending on the game mode (which will update in the class on button click)
